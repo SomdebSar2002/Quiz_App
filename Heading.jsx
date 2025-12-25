3b6f44525330eebaed56fee8649854747b0d384a
SomdebSar2002 <150461759+SomdebSar2002@users.noreply.github.com>
2025-12-25 16:29:31 +0530

    Replace image sources with imported blobs

--- Heading.jsx
+++ Heading.jsx
@@ -1,9 +1,13 @@
+import blob1 from "./blob_1.png"
+import blob2 from "./blob_2.png"
+
+
 export default function Heading(prop) {
     return (<header>
-        <img className="blob2" src="./blob_1.png"/>
+        <img className="blob2" src={blob1}/>
         <h1>Quizzical</h1>
         <p>Your own quiz game</p>
         <button id="game-start" onClick={prop.ok}>Start quiz</button>
-        <img className="blob1" src="./blob_2.png"/>
+        <img className="blob1" src={blob2}/>
     </header>)
 }
